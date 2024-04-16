18dc352955d89783d901c30295d6c186c578fe81
nikhil <nikhil.sharma@telus.com>
2024-04-16 16:47:57 +0530

    new changes

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-import setuptools
+import sys
 
-setuptools.setup(
-    name="anyprint",
-    version="0.1.0",
-    url="https://github.com/kragniz/anyprint",
+from setuptools import setup
+from setuptools_scm import get_version
 
-    author="Louis Taylor",
-    author_email="louis@kragniz.eu",
+# Place the directory containing _version_git on the path
+sys.path.insert(0, "test")
 
-    description="Use any language's print statements in Python",
-    long_description=open('README.md').read(),
+# Use the version module to guess the next development version
+version_string = get_version(
+)
 
-    py_modules=['anyprint'],
-
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-    ],
+setup(
+    version=version_string,
+    setup_requires=["setuptools_scm"],
 )
